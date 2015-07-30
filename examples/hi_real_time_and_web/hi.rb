ec7ced1d0b1b805549a5dec94b3b605a57963384
dB <dblock@dblock.org>
2015-07-30 11:28:59 +0200

    Display domain, user and team name on hello.

--- examples/hi_real_time_and_web/hi.rb
+++ examples/hi_real_time_and_web/hi.rb
@@ -8,7 +8,7 @@ end
 client = Slack::RealTime::Client.new
 
 client.on :hello do
-  puts 'Successfully connected.'
+  puts "Successfully connected, welcome '#{client.self['name']}' to the '#{client.team['name']}' team at https://#{client.team['domain']}.slack.com."
 end
 
 client.on :message do |data|
