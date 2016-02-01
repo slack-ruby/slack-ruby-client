4b786d1c1d6096d6409e5dcb7f41960586cdbf8a
dblock <dblock@dblock.org>
2016-02-01 14:51:03 -0500

    Implemented bot_added and bot_changed.

--- spec/slack/real_time/event_handlers/team_spec.rb
+++ spec/slack/real_time/event_handlers/team_spec.rb
@@ -11,58 +11,48 @@ RSpec.describe Slack::RealTime::Client, vcr: { cassette_name: 'web/rtm_start' } 
       expect(client.team['prefs']['invites_only_admins']).to be true
       expect(client.team['plan']).to eq ''
     end
-    describe 'team_domain_change' do
-      it 'updates team in store' do
-        event = Slack::RealTime::Event.new(
-          'type' => 'team_domain_change',
-          'url' => 'https://my.slack.com',
-          'domain' => 'my'
-        )
-        client.send(:dispatch, event)
-        expect(client.team['domain']).to eq 'my'
-        expect(client.team['url']).to eq 'https://my.slack.com'
-      end
+    it 'team_domain_change' do
+      event = Slack::RealTime::Event.new(
+        'type' => 'team_domain_change',
+        'url' => 'https://my.slack.com',
+        'domain' => 'my'
+      )
+      client.send(:dispatch, event)
+      expect(client.team['domain']).to eq 'my'
+      expect(client.team['url']).to eq 'https://my.slack.com'
     end
-    describe 'email_domain_changed' do
-      it 'updates team in store' do
-        event = Slack::RealTime::Event.new(
-          'type' => 'email_domain_changed',
-          'email_domain' => 'example.com'
-        )
-        client.send(:dispatch, event)
-        expect(client.team['email_domain']).to eq 'example.com'
-      end
+    it 'email_domain_changed' do
+      event = Slack::RealTime::Event.new(
+        'type' => 'email_domain_changed',
+        'email_domain' => 'example.com'
+      )
+      client.send(:dispatch, event)
+      expect(client.team['email_domain']).to eq 'example.com'
     end
-    describe 'team_pref_change' do
-      it 'updates team in store' do
-        event = Slack::RealTime::Event.new(
-          'type' => 'team_pref_change',
-          'name' => 'invites_only_admins',
-          'value' => false
-        )
-        client.send(:dispatch, event)
-        expect(client.team['prefs']['invites_only_admins']).to be false
-      end
+    it 'team_pref_change' do
+      event = Slack::RealTime::Event.new(
+        'type' => 'team_pref_change',
+        'name' => 'invites_only_admins',
+        'value' => false
+      )
+      client.send(:dispatch, event)
+      expect(client.team['prefs']['invites_only_admins']).to be false
     end
-    describe 'team_rename' do
-      it 'updates team in store' do
-        event = Slack::RealTime::Event.new(
-          'type' => 'team_rename',
-          'name' => 'New Team Name Inc.'
-        )
-        client.send(:dispatch, event)
-        expect(client.team['name']).to eq 'New Team Name Inc.'
-      end
+    it 'team_rename' do
+      event = Slack::RealTime::Event.new(
+        'type' => 'team_rename',
+        'name' => 'New Team Name Inc.'
+      )
+      client.send(:dispatch, event)
+      expect(client.team['name']).to eq 'New Team Name Inc.'
     end
-    describe 'team_plan_change' do
-      it 'updates team in store' do
-        event = Slack::RealTime::Event.new(
-          'type' => 'team_plan_change',
-          'plan' => 'std'
-        )
-        client.send(:dispatch, event)
-        expect(client.team['plan']).to eq 'std'
-      end
+    it 'team_plan_change' do
+      event = Slack::RealTime::Event.new(
+        'type' => 'team_plan_change',
+        'plan' => 'std'
+      )
+      client.send(:dispatch, event)
+      expect(client.team['plan']).to eq 'std'
     end
   end
 end
