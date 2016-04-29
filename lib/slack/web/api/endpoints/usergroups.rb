2a9fb48ece1e0b8f0a4411f65c8ccd83b6590930
Tomo Nakano <tomo@quipper.com>
2016-04-29 15:23:13 +0100

    add usergroups_users_list api

--- lib/slack/web/api/endpoints/usergroups.rb
+++ lib/slack/web/api/endpoints/usergroups.rb
@@ -90,6 +90,19 @@ module Slack
             post('usergroups.update', options)
           end
 
+          # This method returns a list of all users within a User Group.
+          #
+          # @option options [Object] :usergroup
+          #   The encoded ID of the User Group to update.
+          # @option options [Object] :include_disabled
+          #   Allow results that involve disabled User Groups.
+          # @see https://api.slack.com/methods/usergroups.users.list
+          # @see https://github.com/dblock/slack-api-ref/blob/master/methods/usergroups/usergroups.users.list.json
+          def usergroups_users_list(options = {})
+            throw ArgumentError.new('Required arguments :usergroup missing') if options[:usergroup].nil?
+            post('usergroups.users.list', options)
+          end
+
           #
           # This method updates the list of users that belong to a User Group. This method replaces all users in a User Group with the list of users provided in the users parameter.
           #
