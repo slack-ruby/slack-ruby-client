c0aa49bc0e3bb6c1edc05a414d851be4b99a0b6b
dblock <dblock@dblock.org>
2017-04-30 11:27:31 -0400

    Fix #146: ThreadError: Target thread must not be current thread and undefined method running?.

--- spec/slack/real_time/concurrency/celluloid_spec.rb
+++ spec/slack/real_time/concurrency/celluloid_spec.rb
@@ -7,20 +7,41 @@ begin
     context 'with url' do
       let(:url) { 'wss://echo.websocket.org/websocket/xyz' }
       let(:logger) { ::Logger.new(STDOUT) }
-      subject(:socket) { described_class.new(url, ping: 42, logger: logger) }
+      let(:test_socket) do
+        Class.new(described_class) do
+          def read
+            fail EOFError
+          end
+        end
+      end
+      let(:socket) { test_socket.new(url, ping: 42, logger: logger) }
       let(:driver) { WebSocket::Driver::Client }
       let(:ws) { double(driver) }
+      subject { socket }
 
       describe '#connect!' do
         pending 'connects'
         pending 'pings every 30s'
       end
 
-      describe '#disconnect!' do
-        it 'closes and nils the websocket' do
+      context 'with a driver' do
+        before do
           socket.instance_variable_set('@driver', ws)
-          expect(ws).to receive(:close)
-          socket.disconnect!
+        end
+
+        describe '#disconnect!' do
+          it 'closes and nils the websocket' do
+            expect(ws).to receive(:close)
+            socket.disconnect!
+          end
+        end
+
+        describe '#run_loop' do
+          it 'runs' do
+            expect(ws).to receive(:emit)
+            expect(ws).to receive(:start)
+            socket.run_loop
+          end
         end
       end
 
