b75bfcb7761ef5443e60f56536eb2f9ca739ddaf
Jeff Manian <jeffrey.manian@gmail.com>
2017-09-18 12:57:42 -0400

    respect rate limits during pagination by sleeping

--- lib/slack/web/pagination/cursor.rb
+++ lib/slack/web/pagination/cursor.rb
@@ -19,11 +19,15 @@ module Slack
             next_cursor = nil
             loop do
               query = { limit: client.default_page_size }.merge(params).merge(cursor: next_cursor)
-              response = client.send(verb, query)
-              yield response
-              break unless response.response_metadata
-              next_cursor = response.response_metadata.next_cursor
-              break if next_cursor.blank?
+              begin
+                response = client.send(verb, query)
+                yield response
+                break unless response.response_metadata
+                next_cursor = response.response_metadata.next_cursor
+                break if next_cursor.blank?
+              rescue Slack::Web::Api::Errors::TooManyRequestsError => e
+                sleep(e.retry_after.seconds)
+              end
             end
           end
         end
