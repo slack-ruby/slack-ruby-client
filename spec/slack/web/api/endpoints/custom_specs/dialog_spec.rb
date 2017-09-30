1aaf5d4b4cd3415962d9d8b380dd2abe5c3bd23c
Alex Agranov <alex@morphogenic.net>
2017-09-29 23:11:08 -0400

    Update for dialogs (#173)
    * initial commit of generated dialog.rb before running git diff to produce patch
    
    * adding Dialog support
    
    * removing the auto-generated dialog spec

---
+++ spec/slack/web/api/endpoints/custom_specs/dialog_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe Slack::Web::Api::Endpoints::Dialog do
+  let(:client) { Slack::Web::Client.new }
+  context 'dialog_open' do
+    it 'automatically converts dialog into JSON' do
+      expect(client).to receive(:post).with(
+        'dialog.open',
+        trigger_id: '12345.98765.abcd2358fdea',
+        dialog: '[]'
+      )
+      client.dialog_open(trigger_id: '12345.98765.abcd2358fdea', dialog: [])
+    end
+
+    context 'arguments' do
+      it 'requires dialog' do
+        expect { client.dialog_open(trigger_id: '123') }.to raise_error ArgumentError, /Required arguments :dialog missing/
+      end
+      it 'requires trigger_id' do
+        expect { client.dialog_open(dialog: []) }.to raise_error ArgumentError, /Required arguments :trigger_id missing/
+      end
+      it 'likes both dialog and trigger_id' do
+        expect(client).to receive(:post).with('dialog.open', hash_including(trigger_id: '123', dialog: '[]'))
+        expect { client.dialog_open(dialog: [], trigger_id: '123') }.to_not raise_error
+      end
+    end
+  end
+end
