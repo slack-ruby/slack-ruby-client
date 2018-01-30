d384b8a400500da6a1ac8e24f0f0223b42c1f864
Vishal Banthia <vishal.banthia.vb@gmail.com>
2018-01-29 17:48:53 -0800

    Improve error message if error response has resonse_metadata

--- lib/slack/web/faraday/response/raise_error.rb
+++ lib/slack/web/faraday/response/raise_error.rb
@@ -8,6 +8,9 @@ module Slack
               raise Slack::Web::Api::Errors::TooManyRequestsError, env.response
             elsif (body = env.body) && !body['ok']
               error_message = body['error'] || body['errors'].map { |message| message['error'] }.join(',')
+              if body.has_key?('response_metadata') && body['response_metadata'].has_key?('messages')
+                error_message << " Response Metadata: #{body['response_metadata']['messages'].join(',')}"
+              end
               raise Slack::Web::Api::Errors::SlackError.new(error_message, env.response)
             end
           end
