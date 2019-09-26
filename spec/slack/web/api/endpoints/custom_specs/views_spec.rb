8235feb78ad2084e309f17897bbf985de97e2c9c
Jeff Manian <jeffrey.manian@gmail.com>
2019-09-26 19:36:47 -0400

    changes for rubocop

--- spec/slack/web/api/endpoints/custom_specs/views_spec.rb
+++ spec/slack/web/api/endpoints/custom_specs/views_spec.rb
@@ -3,69 +3,63 @@ require 'spec_helper'
 
 RSpec.describe Slack::Web::Api::Endpoints::Views do
   let(:client) { Slack::Web::Client.new }
+  let(:trigger_id) { '12345.98765.abcd2358fdea' }
 
-  context 'views_open' do
+  describe 'views_open' do
     context 'with a hash for view' do
+      let(:view_str) { '{"celery":"man"}' }
+
       it 'automatically converts view into JSON' do
-        expect(client).to receive(:post).with(
-          'views.open',
-          trigger_id: '12345.98765.abcd2358fdea',
-          view: '{"celery":"man"}'
-        )
-        client.views_open(trigger_id: '12345.98765.abcd2358fdea', view: {celery: 'man'})
+        expect(client).to receive(:post).with('views.open', trigger_id: trigger_id, view: view_str)
+        client.views_open(trigger_id: trigger_id, view: { celery: 'man' })
       end
     end
+
     context 'with a string for view' do
+      let(:view_str) { 'celery man' }
+
       it 'leaves view as is' do
-        expect(client).to receive(:post).with(
-          'views.open',
-          trigger_id: '12345.98765.abcd2358fdea',
-          view: 'celery man'
-        )
-        client.views_open(trigger_id: '12345.98765.abcd2358fdea', view: 'celery man')
+        expect(client).to receive(:post).with('views.open', trigger_id: trigger_id, view: view_str)
+        client.views_open(trigger_id: trigger_id, view: 'celery man')
       end
     end
   end
 
-  context 'views_push' do
+  describe 'views_push' do
     context 'with a hash for view' do
+      let(:view_str) { '{"celery":"man"}' }
+
       it 'automatically converts view into JSON' do
-        expect(client).to receive(:post).with(
-          'views.push',
-          trigger_id: '12345.98765.abcd2358fdea',
-          view: '{"celery":"man"}'
-        )
-        client.views_push(trigger_id: '12345.98765.abcd2358fdea', view: {celery: 'man'})
+        expect(client).to receive(:post).with('views.push', trigger_id: trigger_id, view: view_str)
+        client.views_push(trigger_id: trigger_id, view: { celery: 'man' })
       end
     end
+
     context 'with a string for view' do
+      let(:view_str) { 'celery man' }
+
       it 'leaves view as is' do
-        expect(client).to receive(:post).with(
-          'views.push',
-          trigger_id: '12345.98765.abcd2358fdea',
-          view: 'celery man'
-        )
-        client.views_push(trigger_id: '12345.98765.abcd2358fdea', view: 'celery man')
+        expect(client).to receive(:post).with('views.push', trigger_id: trigger_id, view: view_str)
+        client.views_push(trigger_id: trigger_id, view: 'celery man')
       end
     end
   end
 
-  context 'views_update' do
+  describe 'views_update' do
     context 'with a hash for view' do
+      let(:view_str) { '{"celery":"man"}' }
+
       it 'automatically converts view into JSON' do
-        expect(client).to receive(:post).with(
-          'views.update',
-          view: '{"celery":"man"}'
-        )
-        client.views_update(view: {celery: 'man'})
+        expect(client).to receive(:post).with('views.update', view: view_str)
+        client.views_update(view: { celery: 'man' })
       end
     end
+
     context 'with a string for view' do
+      let(:view_str) { 'celery man' }
+
       it 'leaves view as is' do
-        expect(client).to receive(:post).with(
-          'views.update',
-          view: 'celery man'
-        )
+        expect(client).to receive(:post).with('views.update', view: view_str)
         client.views_update(view: 'celery man')
       end
     end
