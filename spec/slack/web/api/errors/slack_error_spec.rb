2f5d83c11c21c6737c303c7a33729b4747525189
Jeff Manian <jeffrey.manian@gmail.com>
2020-03-10 14:28:52 -0400

    restore original test an addition to new one

--- spec/slack/web/api/errors/slack_error_spec.rb
+++ spec/slack/web/api/errors/slack_error_spec.rb
@@ -4,7 +4,20 @@ require 'spec_helper'
 RSpec.describe Slack::Web::Api::Errors::SlackError do
   let(:client) { Slack::Web::Client.new }
 
-  it 'provides access to useful info', vcr: { cassette_name: 'web/views_open_error' } do
+  it 'provides access to the response object', vcr: { cassette_name: 'web/auth_test_error' } do
+    begin
+      client.auth_test
+      raise 'Expected to receive Slack::Web::Api::Errors::SlackError.'
+    rescue described_class => e
+      expect(e.response).not_to be_nil
+      expect(e.response.status).to eq 200
+      expect(e.message).to eql 'not_authed'
+      expect(e.error).to eql 'not_authed'
+      expect(e.response_metadata).to be_nil
+    end
+  end
+
+  it 'provides access to response_metadata when available', vcr: { cassette_name: 'web/views_open_error' } do
     begin
       client.views_open(trigger_id: 'trigger_id', view: {})
       raise 'Expected to receive Slack::Web::Api::Errors::SlackError.'
