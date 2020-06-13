1e5f67934043c910b18439fdff466e1ecb09dfbc
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-06-13 10:57:09 +0800

    Add more new methods

--- lib/slack/web/api/endpoints.rb
+++ lib/slack/web/api/endpoints.rb
@@ -15,6 +15,7 @@ require_relative 'endpoints/admin_teams'
 require_relative 'endpoints/admin_teams_admins'
 require_relative 'endpoints/admin_teams_owners'
 require_relative 'endpoints/admin_teams_settings'
+require_relative 'endpoints/admin_usergroups'
 require_relative 'endpoints/admin_users'
 require_relative 'endpoints/admin_users_session'
 require_relative 'endpoints/api'
@@ -25,6 +26,8 @@ require_relative 'endpoints/apps_permissions_scopes'
 require_relative 'endpoints/apps_permissions_users'
 require_relative 'endpoints/auth'
 require_relative 'endpoints/bots'
+require_relative 'endpoints/calls'
+require_relative 'endpoints/calls_participants'
 require_relative 'endpoints/channels'
 require_relative 'endpoints/chat'
 require_relative 'endpoints/chat_scheduledMessages'
@@ -70,6 +73,7 @@ module Slack
         include AdminAppsRequests
         include AdminAppsRestricted
         include AdminConversations
+        include AdminConversationsWhitelist
         include AdminEmoji
         include AdminInviterequests
         include AdminInviterequestsApproved
@@ -78,6 +82,7 @@ module Slack
         include AdminTeamsAdmins
         include AdminTeamsOwners
         include AdminTeamsSettings
+        include AdminUsergroups
         include AdminUsers
         include AdminUsersSession
         include Api
@@ -88,6 +93,8 @@ module Slack
         include AppsPermissionsUsers
         include Auth
         include Bots
+        include Calls
+        include CallsParticipants
         include Channels
         include Chat
         include ChatScheduledmessages
