3b61081b5b9482a6bd5748b050018a050e258db8
Nicholas Koh <wasabigeek@users.noreply.github.com>
2020-07-03 23:07:18 +0800

    Add conversations_id method

--- lib/slack/web/api/mixins.rb
+++ lib/slack/web/api/mixins.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'mixins/channels.id'
+require_relative 'mixins/conversations.id'
 require_relative 'mixins/users.id'
 require_relative 'mixins/users.search'
 require_relative 'mixins/groups.id'
---
+++ spec/slack/web/api/mixins/conversations_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+require 'spec_helper'
+
+RSpec.describe Slack::Web::Api::Mixins::Conversations do
+  subject(:conversations) do
+    klass.new
+  end
+
+  let(:klass) do
+    Class.new do
+      include Slack::Web::Api::Mixins::Conversations
+    end
+  end
+
+  before do
+    allow(conversations).to receive(:conversations_list).and_return(
+      Slack::Messages::Message.new(
+        'channels' => [{
+          'id' => 'CDEADBEEF',
+          'name' => 'general'
+        }]
+      )
+    )
+  end
+
+  context '#conversations_id' do
+    it 'leaves channels specified by ID alone' do
+      expect(conversations.conversations_id(channel: 'C123456')).to(
+        eq('ok' => true, 'channel' => { 'id' => 'C123456' })
+      )
+    end
+    it 'translates a channel that starts with a #' do
+      expect(conversations.conversations_id(channel: '#general')).to(
+        eq('ok' => true, 'channel' => { 'id' => 'CDEADBEEF' })
+      )
+    end
+    it 'fails with an exception' do
+      expect { conversations.conversations_id(channel: '#invalid') }.to(
+        raise_error(Slack::Web::Api::Errors::SlackError, 'channel_not_found')
+      )
+    end
+  end
+end
