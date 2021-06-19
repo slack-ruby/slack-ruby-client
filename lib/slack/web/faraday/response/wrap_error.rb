1f8ce17003f6193c0d6d156e5ca20f4124f9bfaa
Jeff Manian <jeffrey.manian@gmail.com>
2021-06-19 15:20:26 -0400

    raise syntax for rubocop

--- lib/slack/web/faraday/response/wrap_error.rb
+++ lib/slack/web/faraday/response/wrap_error.rb
@@ -9,13 +9,13 @@ module Slack
           def on_complete(env)
             return unless UNAVAILABLE_ERROR_STATUSES.cover?(env.status)
 
-            raise Slack::Web::Api::Errors::UnavailableError.new(env.response)
+            raise Slack::Web::Api::Errors::UnavailableError, env.response
           end
 
           def call(env)
             super
           rescue ::Faraday::TimeoutError, ::Faraday::ConnectionFailed
-            raise Slack::Web::Api::Errors::TimeoutError.new(env.response)
+            raise Slack::Web::Api::Errors::TimeoutError, env.response
           end
         end
       end
