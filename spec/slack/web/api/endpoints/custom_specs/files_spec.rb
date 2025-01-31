64069e26e9b0b9c8c445d7c3961cd2eebe39a7fe
Greg Saab <greg@timescale.com>
2025-01-31 00:05:42 -0500

    add a spec for using all params, rename the valid_params to required_params

--- spec/slack/web/api/endpoints/custom_specs/files_spec.rb
+++ spec/slack/web/api/endpoints/custom_specs/files_spec.rb
@@ -3,43 +3,52 @@ require 'spec_helper'
 
 RSpec.describe Slack::Web::Api::Endpoints::Files do
   let(:client) { Slack::Web::Client.new }
-  let(:valid_params) do
+  let(:required_params) do
     { filename: 'test.txt', content: 'Test File Contents', channels: 'C08AZ76CA4V' }
+  end
+  let(:all_params) do
+    required_params.merge!({ title: 'title', alt_txt: 'alt_txt', snippet_type: 'text', initial_comment: 'initial_comment' })
   end
 
   context 'when filename is missing from options' do
     before do
-      valid_params.delete(:filename)
+      required_params.delete(:filename)
     end
 
     it 'throws argument error' do
-      expect { client.files_upload_v2(valid_params) }.to raise_error ArgumentError
+      expect { client.files_upload_v2(required_params) }.to raise_error ArgumentError
     end
   end
 
   context 'when channels is missing from options' do
     before do
-      valid_params.delete(:channels)
+      required_params.delete(:channels)
     end
 
     it 'throws argument error' do
-      expect { client.files_upload_v2(valid_params) }.to raise_error ArgumentError
+      expect { client.files_upload_v2(required_params) }.to raise_error ArgumentError
     end
   end
 
   context 'when content is missing from options' do
     before do
-      valid_params.delete(:content)
+      required_params.delete(:content)
     end
 
     it 'throws argument error' do
-      expect { client.files_upload_v2(valid_params) }.to raise_error ArgumentError
+      expect { client.files_upload_v2(required_params) }.to raise_error ArgumentError
     end
   end
 
   context 'when all required options are sent', vcr: { cassette_name: 'web/files_upload_v2' } do
     it 'completes the upload' do
-      client.files_upload_v2(valid_params)
+      client.files_upload_v2(required_params)
+    end
+  end
+
+  context 'when all options specified', vcr: { cassette_name: 'web/files_upload_v2_with_all_options' } do
+    it 'completes the upload' do
+      client.files_upload_v2(all_params)
     end
   end
 end
