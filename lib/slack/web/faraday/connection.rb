6792106161ebf0145c989f29b87f5caf537f2574
Greg Saab <greg@timescale.com>
2025-02-03 10:52:46 -0500

    add a connection with no json response parsing

--- lib/slack/web/faraday/connection.rb
+++ lib/slack/web/faraday/connection.rb
@@ -3,36 +3,44 @@ module Slack
   module Web
     module Faraday
       module Connection
-        private
+        def default_options
+          options = {
+            headers: { 'Accept' => 'application/json; charset=utf-8' }
+          }
+          options[:headers]['User-Agent'] = user_agent if user_agent
+          options[:proxy] = proxy if proxy
+          options[:ssl] = { ca_path: ca_path, ca_file: ca_file } if ca_path || ca_file
+
+          request_options = {}
+          request_options[:timeout] = timeout if timeout
+          request_options[:open_timeout] = open_timeout if open_timeout
+          options[:request] = request_options if request_options.any?
+          options
+        end
+
+        def create_connection(expect_json_response: true)
+          options = default_options
+          ::Faraday::Connection.new(endpoint, options) do |connection|
+            connection.request :multipart
+            connection.request :url_encoded
+            connection.use ::Slack::Web::Faraday::Response::RaiseError if expect_json_response
+            connection.response :mashify, mash_class: Slack::Messages::Message
+            connection.response :json, content_type: /\b*$/ if expect_json_response
+            connection.use ::Slack::Web::Faraday::Response::WrapError
+            connection.response :logger, logger if logger
+            connection.adapter adapter
+          end
+        end
 
         def connection
-          @connection ||=
-            begin
-              options = {
-                headers: { 'Accept' => 'application/json; charset=utf-8' }
-              }
-
-              options[:headers]['User-Agent'] = user_agent if user_agent
-              options[:proxy] = proxy if proxy
-              options[:ssl] = { ca_path: ca_path, ca_file: ca_file } if ca_path || ca_file
-
-              request_options = {}
-              request_options[:timeout] = timeout if timeout
-              request_options[:open_timeout] = open_timeout if open_timeout
-              options[:request] = request_options if request_options.any?
-
-              ::Faraday::Connection.new(endpoint, options) do |connection|
-                connection.request :multipart
-                connection.request :url_encoded
-                connection.use ::Slack::Web::Faraday::Response::RaiseError
-                connection.response :mashify, mash_class: Slack::Messages::Message
-                connection.response :json, content_type: /\bjson$/
-                connection.use ::Slack::Web::Faraday::Response::WrapError
-                connection.response :logger, logger if logger
-                connection.adapter adapter
-              end
-            end
+          @connection ||= create_connection
         end
+
+        def connection_without_response_parsing
+          @connection_without_response_parsing ||= create_connection(false)
+        end
+
+        private :create_connection
       end
     end
   end
